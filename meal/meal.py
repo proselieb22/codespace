34286e086552cb8d7bc9b0f92c3d85b2f5d64407
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-10-08 18:16:06 +0000

    Sun, Oct 8, 2023, 6:16 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,11 @@
 def main():
     answer = input("What time is it? ")
     time = convert(answer)
-    if time >= 7 and <= 8:
-        print("breakfast time"
-    if time >= 12 and <= 13:
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+    if time >= 12 and time <= 13:
         print("lunch time")
-    if time >= 18 and <= 19:
+    if time >= 18 and time <= 19:
         print("dinner time")
 def convert(time):
     hours, minutes = time.split(":")
