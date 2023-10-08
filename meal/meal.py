7b530fd16e137b0ccb0e8dc07b4bcf7c5ba9dba9
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-10-08 18:14:55 +0000

    Sun, Oct 8, 2023, 6:14 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,12 @@
 def main():
     answer = input("What time is it? ")
     time = convert(answer)
-    print(time)
+    if time >= 7 and <= 8:
+        print("breakfast time"
+    if time >= 12 and <= 13:
+        print("lunch time")
+    if time >= 18 and <= 19:
+        print("dinner time")
 def convert(time):
     hours, minutes = time.split(":")
     new_minute = float(minutes) / 60
