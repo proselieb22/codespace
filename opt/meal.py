01a904854134fce3887c7eff6ab84126711aa028
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-10-08 18:06:19 +0000

    Sun, Oct 8, 2023, 6:06 PM +00:00

--- opt/meal.py
+++ opt/meal.py
@@ -1,11 +1,11 @@
 def main():
     input = input("What time is it? ")
     time = convert(input)
-
+    print(time)
 def convert(time):
     hours, minutes = time.split(":")
-    new_minute = float(minutes) / 60 
-
+    new_minute = float(minutes) / 60
+    return float(hours) = float(minutes) / 60
 
 if __name__ == "__main__":
     main()
