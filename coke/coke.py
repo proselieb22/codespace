34ec99875e09fb3eb300a62cab9d29ce7baf1c80
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-11-13 21:00:56 +0000

    Mon, Nov 13, 2023, 9:00 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -10,12 +10,13 @@ def main():
             if coin in [25, 10, 5]:
                 total_inserted += coin
             else:
-                print("Invalid coin. Please insert a valid coin (25, 10, or 5 cents).")
+                print("Coke only accepts 25, 10, or 5 cents. Please insert a valid coin.")
+
         change = calculate_change(total_inserted)
-        if change > 0:
-            print(f"Change Owed: {change} cents")
-        else:
+        if change == 0:
             print("No change owed. Thank you!")
+        else:
+            print(f"Change Owed: {change if change > 0 else 0} cents")
 
         choice = input("Would you like to buy another Coke? (yes/no): ")
         if choice.lower() != 'yes':
@@ -23,3 +24,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
