3c6c574bf09b6c7343ee4bab35f4e5ae8ea384f9
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-11-13 21:48:55 +0000

    Mon, Nov 13, 2023, 9:48 PM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,24 +1,24 @@
 fruit_calories = {
     "apple": 130,
     "avocado": 50,
-    "Banana": 110,
-    "Cantaloupe": 50,
-    "Grapefruit": 60,
-    "Grapes": 90,
-    "Honeydew Melon": 50,
-    "Kiwifruit": 90,
-    "Lemon": 15,
-    "Lime": 20,
-    "Nectarine": 60,
-    "Orange": 80,
-    "Peach": 60,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
     "pear": 100,
-    "Pineapple": 50,
-    "Plums": 70,
-    "Strawberries": 50,
-    "Sweet Cherries": 100,
-    "Tangerine": 50,
-    "Watermelon": 80,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
 }
 
 def main():
@@ -28,3 +28,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
