00b9f45399292dbbfd5b85bbcb39bf03eeace9c8
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-11-13 21:13:15 +0000

    Mon, Nov 13, 2023, 9:13 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,28 +2,25 @@ def calculate_change(total_inserted):
     return total_inserted - 50  # Calculate the change
 
 def main():
-    while True:
-        total_inserted = 0
-        while total_inserted < 50:
+    total_inserted = 0
+    while total_inserted < 50:
+        coin = int(input("Insert Coin: "))
+        if coin in [25, 10, 5]:
+            total_inserted += coin
             print(f"Amount Due: {50 - total_inserted}")
-            coin = int(input("Insert Coin: "))
-            if coin in [25, 10, 5]:
-                total_inserted += coin
-            else:
-                print("coke rejects invalid amount of cents")
-
-        change = calculate_change(total_inserted)
-        if change == 0:
-            print("Change Owed: 0 cents")
         else:
-            print(f"Change Owed: {abs(change)} cents")
+            print(f"Amount Due: {50 - total_inserted}")
+            print("Machine does not accept that denomination.")
 
-        choice = input("Would you like to buy another Coke? (yes/no): ")
-        if choice.lower() != 'yes':
-            break
+    change = calculate_change(total_inserted)
+    if change == 0:
+        print("Change Owed: 0")
+    else:
+        print(f"Change Owed: {change if change > 0 else 0}")
 
 if __name__ == "__main__":
     main()
+
 
 
 
