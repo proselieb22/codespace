96a080bd7de417c5c7f17302db683fb52be7ebd8
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-11-13 21:31:05 +0000

    Mon, Nov 13, 2023, 9:31 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,3 +1,27 @@
+def starts_with_letters(s):
+    return s[:2].isalpha()
+
+def valid_length(s):
+    return 2 <= len(s) <= 6
+
+def valid_number_placement(s):
+    if any(char.isdigit() for char in s):
+        last_num_index = s.rfind([char for char in s if char.isdigit()][-1])
+        if last_num_index != -1:
+            return last_num_index == len(s) - 1 and s[last_num_index] != '0'
+    return True
+
+def no_punctuation(s):
+    return s.isalnum()
+
+def is_valid(s):
+    return (
+        starts_with_letters(s) and
+        valid_length(s) and
+        valid_number_placement(s) and
+        no_punctuation(s)
+    )
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -5,13 +29,5 @@ def main():
     else:
         print("Invalid")
 
-def is_valid(s):
-    if len(s) < 2 or len(s) > 6:  # Check length
-        return False
-    if not s[:2].isalpha():  # Check if first two characters are letters
-        return False
-    if s[2:-1].isdigit() and s[-1].isnumeric() and s[2] != '0':  # Check number placement
-        return True
-    return False
-
-main()
+if __name__ == "__main__":
+    main()
