dab80a38a325a77b8e1b71c48016d39d51349317
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-11-13 20:40:07 +0000

    Mon, Nov 13, 2023, 8:40 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,2 +1,18 @@
-for c in s:
-    print(c, end="")
+def camel_to_snake_case(variable_name):
+    snake_case_name = ""
+    for char in variable_name:
+        if char.isupper():
+            snake_case_name += "_" + char.lower()
+        else:
+            snake_case_name += char
+    if snake_case_name[0] == "_":
+        snake_case_name = snake_case_name[1:]
+    return snake_case_name
+
+def main():
+    camel_case_var = input("Enter a variable name in camel case: ")
+    snake_case_var = camel_to_snake_case(camel_case_var)
+    print(f"The variable name in snake case is: {snake_case_var}")
+
+if __name__ == "__main__":
+    main()
