2d11e28073139524ac6fe1f0bbad1a2eaf4172b2
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-12-05 12:08:43 +0000

    Tue, Dec 5, 2023, 12:08 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,28 @@
-try:
-Fraction = print("Fraction: ")
-if fraction = int((fraction)):
-    print("Fraction: ")
-except:(ValueError, ZeroDivisionError):
-    print("fraction is not an integer")
+def calculate_fuel(fraction):
+    try:
+        x, y = map(int, fraction.split('/'))
+        percentage = (x / y) * 100
+
+        if percentage <= 1:
+            return 'E'
+        elif percentage >= 99:
+            return 'F'
+        else:
+            return f'{round(percentage)}%'
+
+    except ValueError:
+        return "Please enter the fraction in the format X/Y, where X and Y are integers."
+    except ZeroDivisionError:
+        return "Cannot divide by zero. Please provide a valid denominator."
+
+def main():
+    while True:
+        fraction = input("Enter the fraction (X/Y): ")
+        result = calculate_fuel(fraction)
+        print(result)
+        if result in ['E', 'F']:
+            break
+
+if __name__ == "__main__":
+    main()
+
