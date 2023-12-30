2cf4f4e7bf462ff0cf7a3b4bc1e4336167f5138a
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-12-30 13:41:59 +0000

    Sat, Dec 30, 2023, 1:41 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,46 +1,55 @@
+from sys import argv, exit
 import os
-import sys
-from PIL import Image, ImageOps
+from PIL import ImageOps as imgops, Image as img
 
-def overlay_shirt(input_image, output_image):
-    # Check if input and output files have correct extensions
-    valid_extensions = ['.jpg', '.jpeg', '.png']
-    input_extension = os.path.splitext(input_image)[1].lower()
-    output_extension = os.path.splitext(output_image)[1].lower()
+def main():
+    inpt, outpt = get_filenames(argv)
+    check_ext(inpt, outpt)
 
-    if input_extension not in valid_extensions or output_extension not in valid_extensions:
-        sys.exit("Invalid input/output file format")
-
-    # Check if input and output extensions match
-    if input_extension != output_extension:
-        sys.exit("Input and output have different extensions")
-
-    # Check if input file exists
-    if not os.path.exists(input_image):
-        sys.exit("Input does not exist")
-
-    # Open input and shirt images
+    # open images
     try:
-        input_img = Image.open(input_image)
-        shirt = Image.open("shirt.png")  # Replace "shirt.png" with the actual shirt image file name
-
-        # Resize input image to the same size as shirt image
-        input_img = input_img.resize(shirt.size)
-
-        # Overlay shirt on input image
-        input_img.paste(shirt, (0, 0), shirt)
-
-        # Save the result
-        input_img.save(output_image)
-        print(f"Shirt overlay saved as {output_image}")
+        shirt = img.open('shirt.png')
+        muppet = img.open(inpt)
     except FileNotFoundError:
-        sys.exit("Could not open one of the images")
+        exit('Input does not exist')
+    except Exception as exp:
+        exit(exp)
+    # get size of shirt.png
+    size = shirt.size
+    # crop muppet to fit shirt
+    muppet = imgops.fit(muppet, size)
+    # overlay shirt on mupper
+    muppet.paste(shirt, shirt)
+    # save image
+    muppet.save(outpt)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("Too few or too many command-line arguments")
+def get_filenames(arguments):
+    if len(arguments) != 3:
+        if len(arguments) < 3:
+            exit('Too few command-line arguments')
+        else:
+            exit('Too mant command-line arguments')
+    return [argv[1],argv[2]]
 
-    input_image = sys.argv[1]
-    output_image = sys.argv[2]
+def check_ext(inpt, outpt):
+    in_root, in_ext = os.path.splitext(inpt)
+    out_root, out_ext = os.path.splitext(outpt)
 
-    overlay_shirt(input_image, output_image)
+    # check input and output have same extensions
+    if in_ext.lower() != out_ext.lower():
+        exit('Input and output have different extensions')
+
+    #check valid input
+    if in_ext.lower() == '.jpg' or in_ext.lower() == '.jpeg' or in_ext.lower() == '.png':
+        pass
+    else:
+        exit('Invalid input')
+
+    #check valid output
+    if out_ext.lower() == '.jpg' or out_ext.lower() == '.jpeg' or out_ext.lower() == '.png':
+        pass
+    else:
+        exit('Invalid output')
+
+if __name__ == '__main__':
+    main()
