d81981f93305cec4128aaaea86fb43f23b2cb57b
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-12-30 14:00:27 +0000

    Sat, Dec 30, 2023, 2:00 PM +00:00

---
+++ um/test_um.py
@@ -0,0 +1,17 @@
+from um import count
+
+
+def test_single_um():
+    assert count("um") == 1
+
+
+def test_question_um():
+    assert count("um?") == 1
+
+
+def test_um_in_sentence():
+    assert count("Um, thanks for the album.") == 1
+
+
+def test_multiple_um():
+    assert count("Um, thanks, um...") == 2
---
+++ um/um.py
@@ -0,0 +1,16 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    pattern = r'\bum\b'
+    matches = re.findall(pattern, s, flags=re.IGNORECASE)
+    return len(matches)
+
+
+if __name__ == "__main__":
+    main()
