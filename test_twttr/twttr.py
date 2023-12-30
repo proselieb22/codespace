96d878f9d8663327af323f382a83dd46ba73faad
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-12-30 12:57:49 +0000

    Sat, Dec 30, 2023, 12:57 PM +00:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,3 +1,16 @@
 def main():
-    text = input("tex")
-    shorten()
+    input("input: ")
+    input_without_vowels = shorten(input)
+    print("Output: input_without_vowels)
+
+def shorten(word):
+    word_without_vowels = ""
+    for letter in word:
+        if not letter.lower() in ["a", "e", "i", "o", "u"]:
+            word_without_voweös += letter
+    return word_without_vowels
+
+if _name_ == "__main__":
+    main()
+
+
