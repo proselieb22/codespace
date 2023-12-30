e67f72c66886f58b5b9013f1405d25e1ffa7b1f2
proselieb22 <122692946+proselieb22@users.noreply.github.com>
2023-12-30 22:20:19 +0000

    Sat, Dec 30, 2023, 10:20 PM +00:00

--- project/project.py
+++ project/project.py
@@ -1,109 +1,109 @@
-import sys
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+import customtkinter
+import json
 import requests
-from bs4 import BeautifulSoup as BS
-import bs4
-import re
-import wikipedia
 
+location_label = object
+image = object
+temperature = object
+weather = object
+api = "524b382968907ae30c822c0cc869ac26"
+
+def gui():
+    frame = Tk()
+    frame.title("Weather app")
+    frame.iconbitmap("icons\weather_icon.ico")
+
+    # get the screen dimentions of a laptop
+    screen_width = frame.winfo_screenwidth()
+    screen_height = frame.winfo_screenheight()
+
+    # centering the window
+    center_width = int(screen_width/2 - 500/2)  # 500 is a frame dimentions
+    center_height = int(screen_height/2 - 500/2)  # 500 is frame dimentions
+
+    frame.geometry(f"500x500+{center_width}+{center_height}")
+    frame.resizable(False, False)
+    frame.attributes("-alpha", 0.94)  # making transparent for 94 percents
+
+    frame.configure(background="#fffbd5")
+
+    city_input = StringVar()
+    input = customtkinter.CTkEntry(frame, textvariable=city_input, width=360)
+    input.place(relx=0.5, rely=0.2, anchor=CENTER)
+    input.focus_set()
+    input.pack()
+
+    search_button = customtkinter.CTkButton(master=frame, text="Search", width=260, command=lambda: search(city_input))
+    search_button.pack()
+
+    # labels
+    global location_label
+    location_label = Label(frame, text='', font=('bold', 20))
+    location_label.config(background="#fffbd5")
+    location_label.pack()
+
+    global image
+    canvas = Canvas(frame, width=100, height=100)
+    canvas.config(background="#fffbd5")
+    canvas.pack()
+    image = PhotoImage(file="")
+    canvas.create_image(50, 50, anchor=CENTER, image=image)
+
+    global temperature
+    temperature = Label(frame, text='')
+    temperature.config(font=14, background="#fffbd5")
+    temperature.pack()
+
+    global weather
+    weather = Label(frame, text='')
+    weather.config(font=14, background="#fffbd5")
+    weather.pack()
+
+    # Execute Tkinter
+    frame.mainloop()
+
+def get_coordinates(city: str)-> None:
+    url = "http://api.openweathermap.org/geo/1.0/direct?q={}&limit={}&appid={}"
+
+    request = requests.get(url.format(city, 1, api))
+    if request:
+        json = request.json()
+        # print(json[0]["lat"], json[0]["lon"])
+        # print(get_weather(json[0]["lat"], json[0]["lon"]))
+        return get_weather(json[0]["lat"], json[0]["lon"])
+
+def get_weather(lat: str, lon: str)-> tuple:
+    get_city_url = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
+
+    result = requests.get(get_city_url.format(lat, lon, api))
+    if result:
+        json = result.json()
+        city = json["name"]
+        weather = json["weather"][0]["main"]
+        icon = json["weather"][0]["icon"]
+        temperature_kelvin = json["main"]["temp"]
+        temperature_celsius = temperature_kelvin - 273.15
+        temperature_fahrenheit = (temperature_kelvin - 273.15) * 9 / 5 + 32
+        country = json["sys"]["country"]
+
+        return  (city, weather, icon, temperature_celsius, temperature_fahrenheit, country)
+
+def search(city_input):
+    city = city_input.get()
+    weather_tuple = get_coordinates(city)
+    if weather_tuple:
+        location_label['text'] = "{}, {}".format(weather_tuple[0], weather_tuple[5])
+        image["file"] = "icons\{}.png".format(weather_tuple[2])
+        temperature["text"] = "{}°C, {}°F".format(round(float(weather_tuple[3]), 2), round(float(weather_tuple[4])), 2)
+        weather["text"] = weather_tuple[1]
+    else:
+        messagebox.showerror(title="Error", message=f"City {city} is not found")
 
 def main():
-    # usage
-    if sys.argv[1] == "-h":
-        print("This program helps you sort key words by number of google results.")
-        print("Usage: project.py INPUT_FILE OUTPUT_FILE [-w]")
-        print("-h  help")
-        print("-w  output links for wikipedia pages into the file")
-        sys.exit(0)
-    elif len(sys.argv) == 3:
-        w = False
-    elif len(sys.argv) == 4 and sys.argv[-1] == '-w':
-        w = True
-    else:
-        print("Usage: project.py INPUT_FILE OUTPUT_FILE [-w]")
-        print("Use -h for the help page")
-        sys.exit(1)
-
-    # google search
-    try:
-        f1 = open(sys.argv[1])
-    except FileNotFoundError:
-        print("Invalid input file")
-        sys.exit(1)
-    else:
-        results = google(f1.readlines())
-        f1.close()
-
-        if w:
-            f1 = open(sys.argv[1])
-            wikipages = wiki(f1.readlines())
-            f1.close()
-        else:
-            wikipages = {}
-
-    # output file
-    output(results, wikipages, sys.argv[2])
-
-
-def test_google():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
-    response = requests.get("https://www.google.com/search?q=Ecoli", headers=headers)
-    match = re.search(r"E\s*?coli", response.text, re.IGNORECASE)  # Updated regex pattern
-    assert match is not None
-
-        if stats:
-            matches = re.search(r"About ([0-9,]+) results", stats.text)
-            if matches:
-                n = matches.group(1)
-            else:
-                n = '0'
-        else:
-            n = '0'
-        results[line.strip()] = int(n.replace(',', ''))
-
-    return results
-
-def wiki(file):
-    '''
-    Input a file with key words in each line.
-    Output a dictionary with key words as keys, wikipedia links as values.
-    '''
-    wikipages = {}
-
-    for line in file:
-        # search the key word on wikipedia
-        try:
-            word = wikipedia.search(line.strip())[0]
-        except IndexError:
-            continue
-
-        # get the wikipage
-        try:
-            page = wikipedia.page(word, auto_suggest=False)
-        except wikipedia.exceptions.DisambiguationError as e:
-            word = e.options[0]
-            page = wikipedia.page(word, auto_suggest=False)
-
-        wikipages[line.strip()] = page.url
-
-    return wikipages
-
-
-def output(results, wikipages, outfile="output.tsv"):
-    '''
-    Input dictionaries with key words and their numbers of google search results or wikipedia links.
-    Input a filename.
-    Output the key words in a descending order based on numbers of google search results, together with wikipedia links (if exist).
-    '''
-    sort_results = dict(sorted(results.items(), key=lambda x: x[1], reverse=True))
-
-    with open(outfile, 'w') as f2:
-        for key, value in sort_results.items():
-            f2.write(key + "\t" + str('{:,}'.format(value)))
-            if key in wikipages:
-                f2.write("\t" + wikipages[key] + "\n")
-            else:
-                f2.write("\n")
-
+    gui()
 
 if __name__ == "__main__":
     main()
